b2e96ca6dff71b4b6a2ed817bd9651a1104795fd
gdhamnekar1 <110093373+gdhamnekar1@users.noreply.github.com>
2023-03-23 23:09:16 +0530

    change key name

--- deveopsproject.tf
+++ deveopsproject.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "Ganesh" {
   count ="1"
   ami ="ami-0f8ca728008ff5af4"
   instance_type ="t2.micro"
-  key name ="ganesh71"
+  key name ="ganeshsd72"
   subnet_id = "subnet-03e27f96cca27f839"
    tags = {
     Name = "HelloWorld"
