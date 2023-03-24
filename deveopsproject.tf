183ff6d3026933db500bf5085412c6badf98fc01
gdhamnekar1 <110093373+gdhamnekar1@users.noreply.github.com>
2023-03-24 13:55:20 +0530

    Update deveopsproject.tf

--- deveopsproject.tf
+++ deveopsproject.tf
@@ -1,13 +1,15 @@
 provider "aws" {
-  region= "ap-south-1"
+  region     = "ap-south-1"
+  access_key = "AKIAQZTK7F7PTNF6BZME"
+  secret_key = "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"
 }
 
 resource "aws_instance" "web" {
   ami           = "ami-0f8ca728008ff5af4"
-  instance_type = "t3.micro"
+  instance_type = "t2.micro"
   subnet_id     = "subnet-03e27f96cca27f839"
 
   tags = {
-    Name = "HelloWorld"
+    Name = "HelloWorld36"
   }
 }
